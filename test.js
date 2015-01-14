f3cc6e020acb4ad9661c7ba9a8dab431976baa0a
Dong Weiqiang <weiqiang@gmail.com>
2015-01-14 23:13:57 +0800

    add 3 test case

--- test.js
+++ test.js
@@ -4,16 +4,84 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var asciidoctor = require('./');
 
-it('should compile asciidoctor to HTML', function(cb) {
+it('should compile asciidoctor to HTML', function (cb) {
     var stream = asciidoctor({
         doctype: 'inline',
         attributes: ['showtitle']
     });
 
-    stream.once('data', function(file) {
+    stream.once('data', function (file) {
         assert.equal(file.relative, 'fixture.html');
         assert.equal(file.contents.toString(),
             '<strong>foo</strong>'
+        );
+    });
+
+    stream.on('end', cb);
+
+    stream.write(new gutil.File({
+        path: 'fixture.md',
+        contents: new Buffer('*foo*')
+    }));
+
+    stream.end();
+});
+
+
+it('should run ok without args', function (cb) {
+    var stream = asciidoctor();
+
+    stream.once('data', function (file) {
+        assert.equal(file.relative, 'fixture.html');
+    });
+
+    stream.on('end', cb);
+
+    stream.write(new gutil.File({
+        path: 'fixture.md',
+        contents: new Buffer('*foo*')
+    }));
+
+    stream.end();
+});
+
+
+// header_footer: false,
+it('test case without header_footer', function (cb) {
+    var stream = asciidoctor({
+        header_footer: false,
+        attributes: ['showtitle']
+    });
+
+    stream.once('data', function (file) {
+        assert.equal(file.relative, 'fixture.html');
+        assert.equal(file.contents.toString(),
+            '<div class=\"paragraph\">\n<p><strong>foo</strong></p>\n</div>'
+        );
+    });
+
+    stream.on('end', cb);
+
+    stream.write(new gutil.File({
+        path: 'fixture.md',
+        contents: new Buffer('*foo*')
+    }));
+
+    stream.end();
+});
+
+
+// header_footer: true,
+it('test case with header_footer', function (cb) {
+    var stream = asciidoctor({
+        header_footer: true,
+        attributes: ['showtitle']
+    });
+
+    stream.once('data', function (file) {
+        assert.equal(file.relative, 'fixture.html');
+        assert.notEqual(file.contents.toString(),
+            '<div class=\"paragraph\">\n<p><strong>foo</strong></p>\n</div>'
         );
     });
 
