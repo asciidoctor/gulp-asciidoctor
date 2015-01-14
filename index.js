3d35213e45d8e5895a73b6f1a8c7350ca89fd256
Dong Weiqiang <weiqiang@gmail.com>
2015-01-14 11:50:00 +0800

    fix a bug and update to v1.0.5

--- index.js
+++ index.js
@@ -6,54 +6,59 @@ var opal = asciidoctorJs.Opal;
 var processor = asciidoctorJs.Asciidoctor(true);
 
 module.exports = function (options) {
-  // default config
-  options = options || {};
-  options.base_dir = options.base_dir || process.cwd();
-  options.safe = options.safe || 'secured';
-  options.doctype = options.doctype || 'article'; //article,book,manpage,inline
-  options.header_footer = options.header_footer || true;
-  options.attributes = options.attributes || ['showtitle'];
+    // default config
+    var asciidoctorOptions = {};
 
-  var optionsOpal = opal.hash2(
-    ['base_dir', 'safe', 'doctype', 'header_footer',
-      'attributes'
-    ], options);
-
-  // creating a stream through which each file will pass
-  var stream = through(function (file, encoding, callback) {
-    // do whatever necessary to process the file
-    if (file.isNull()) {
-      callback(null, file);
-      return;
+    asciidoctorOptions.base_dir = options.base_dir || process.cwd();
+    asciidoctorOptions.safe = options.safe || 'secured';
+    asciidoctorOptions.doctype = options.doctype || 'article';
+    asciidoctorOptions.attributes = options.attributes || ['showtitle'];
+    asciidoctorOptions.header_footer = true;
+    if (typeof options.header_footer !== 'undefined') { // the variable is defined
+        asciidoctorOptions.header_footer = !!options.header_footer;
     }
 
-    if (file.isStream()) {
-      callback(new gutil.PluginError('gulp-asciidoctor',
-        'Streaming not supported'));
-      return;
-    }
+    var optionsOpal = opal.hash2(
+        ['base_dir', 'safe', 'doctype', 'header_footer',
+            'attributes'
+        ], asciidoctorOptions);
 
-    if (file.isBuffer()) {
 
-    }
+    // creating a stream through which each file will pass
+    var stream = through(function (file, encoding, callback) {
+        // do whatever necessary to process the file
+        if (file.isNull()) {
+            callback(null, file);
+            return;
+        }
 
-    // just pipe data next, or just do nothing to process file later in flushFunction
-    // never forget callback to indicate that the file has been processed.
+        if (file.isStream()) {
+            callback(new gutil.PluginError('gulp-asciidoctor',
+                'Streaming not supported'));
+            return;
+        }
 
-    var data = processor.$convert(file.contents.toString(),
-      optionsOpal);
+        if (file.isBuffer()) {
 
-    file.contents = new Buffer(data);
-    file.path = gutil.replaceExtension(file.path, '.html');
+        }
 
-    callback(null, file);
+        // just pipe data next, or just do nothing to process file later in flushFunction
+        // never forget callback to indicate that the file has been processed.
 
-  }, function (callback) {
-    // just pipe data next, just callback to indicate that the stream's over
-    // this.push(something);
-    callback();
-  });
+        var data = processor.$convert(file.contents.toString(),
+            optionsOpal);
 
-  // returning the file stream
-  return stream;
+        file.contents = new Buffer(data);
+        file.path = gutil.replaceExtension(file.path, '.html');
+
+        callback(null, file);
+
+    }, function (callback) {
+        // just pipe data next, just callback to indicate that the stream's over
+        // this.push(something);
+        callback();
+    });
+
+    // returning the file stream
+    return stream;
 };
