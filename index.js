dbaba0e58146a44fcb664653dbdb8873e68edc1b
Guillaume Grossetie <ggrossetie@gmail.com>
2016-11-28 19:52:22 +0100

    Resolves #7, Upgrade to Asciidoctor.js 1.5.5-4

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // PLUGIN_NAME: gulp-asciidoctor
 var through = require('through-gulp');
 var gutil = require('gulp-util');
-var asciidoctorJs = require('asciidoctor.js')();
-var opal = asciidoctorJs.Opal;
-var processor = asciidoctorJs.Asciidoctor(true);
+var asciidoctor = require('asciidoctor.js')();
 
 module.exports = function (options) {
 
@@ -11,19 +9,11 @@ module.exports = function (options) {
     var asciidoctorOptions = {};
 
     // default config
-    asciidoctorOptions.base_dir = options.base_dir || process.cwd();
-    // asciidoctorOptions.safe = options.safe || 'secure'; //unsafe, safe, server or secure,
-
     asciidoctorOptions.safe = options.safe || 'secure'; //unsafe, safe, server or secure
     asciidoctorOptions.doctype = options.doctype || 'article'; //book,inline
     asciidoctorOptions.attributes = options.attributes || ['showtitle'];
     asciidoctorOptions.header_footer = (options.header_footer === undefined ?
         true : options.header_footer);
-
-    var optionsOpal = opal.hash2(
-        ['base_dir', 'safe', 'doctype', 'header_footer', 'attributes'],
-        asciidoctorOptions);
-
 
     // creating a stream through which each file will pass
     var stream = through(function (file, encoding, callback) {
@@ -46,8 +36,8 @@ module.exports = function (options) {
         // just pipe data next, or just do nothing to process file later in flushFunction
         // never forget callback to indicate that the file has been processed.
 
-        var data = processor.$convert(file.contents.toString(),
-            optionsOpal);
+        var data = asciidoctor.convert(file.contents.toString(),
+            asciidoctorOptions);
 
         file.contents = new Buffer(data);
         file.path = gutil.replaceExtension(file.path, '.html');
